e771dfa38538f2215821003a3fa0539d41869123
ZinovkinaSvetlana <pidgin66@gmail.com>
2024-10-12 22:18:06 +0300

    change dependencies

---
+++ Makefile
@@ -0,0 +1,8 @@
+i:
+	npm install
+
+start:
+	npm run start
+
+format:
+	npx prettier --write .
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        comfortaa: ['Comfortaa', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
